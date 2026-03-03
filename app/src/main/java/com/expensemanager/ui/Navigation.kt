aa6b964c7364ba6dc9a17f92fcc27ab57918438e
Rajeev Bansal <rajivbansal36325@gmail.com>
2026-03-03 19:42:18 +0530

    Update Navigation.kt

--- app/src/main/java/com/expensemanager/ui/Navigation.kt
+++ app/src/main/java/com/expensemanager/ui/Navigation.kt
@@ -41,120 +41,82 @@ object Routes {
     fun accountRecords(accountId: Long) =
         "account_records/$accountId"
 }
-
 @Composable
 fun AppNavGraph(navController: NavHostController) {
+
     NavHost(
         navController = navController,
         startDestination = Routes.DASHBOARD
     ) {
+
         composable(Routes.DASHBOARD) {
             DashboardScreen(
                 onAddTransaction = {
                     navController.navigate(Routes.addTransaction())
                 },
-            
                 onTransactionClick = { id ->
                     navController.navigate(Routes.addTransaction(id))
                 },
-            
                 onAccountsClick = {
                     navController.navigate(Routes.ACCOUNTS)
                 },
-            
-                onAccountDetailsClick = { accountId ->
-                    navController.navigate(
-                        Routes.accountDetails(accountId)
-                    )
-                },
-            
-                onAccountRecordsClick = { accountId ->
-                    navController.navigate(
-                        Routes.accountRecords(accountId)
-                    )
-                },
-            
                 onImportClick = {
                     navController.navigate(Routes.IMPORT)
                 },
-            
                 onSettingsClick = {
                     navController.navigate(Routes.SETTINGS)
                 }
             )
         }
-        composable(
-            route = Routes.ADD_TRANSACTION,
-            arguments = listOf(
-                navArgument("transactionId") {
-                    type = NavType.LongType
-                    defaultValue = -1L
-                }
-            )
-        ) { backStack ->
-            val transactionId = backStack.arguments?.getLong("transactionId")?.takeIf { it != -1L }
-            AddTransactionScreen(
-                transactionId = transactionId,
-                onBack = { navController.popBackStack() }
-            )
-        }
+
         composable(Routes.ACCOUNTS) {
             AccountsScreen(
                 onBack = { navController.popBackStack() },
-                onAddTransaction = { accountId ->
-                    navController.navigate(Routes.addTransaction())
-                }
-            )
-        }
-        composable(
-            route = Routes.ACCOUNT_DETAILS,
-            arguments = listOf(
-                navArgument("accountId") {
-                    type = NavType.LongType
-                }
-            )
-        ) { backStack ->
-        
-            val accountId =
-                backStack.arguments!!.getLong("accountId")
-        
-            AccountDetailScreen(
-                accountId = accountId,
-                onBack = {
-                    navController.popBackStack()
-                }
-            )
-        }
-        composable(
-            route = Routes.ACCOUNT_RECORDS,
-            arguments = listOf(
-                navArgument("accountId") {
-                    type = NavType.LongType
-                }
-            )
-        ) { backStack ->
-        
-            val accountId =
-                backStack.arguments!!.getLong("accountId")
-        
-            RecordsScreen(
-                accountId = accountId,
-                onBack = {
-                    navController.popBackStack()
+                onAddAccount = {
+                    navController.navigate(Routes.ADD_ACCOUNT)
                 },
-                onTransactionClick = { transactionId ->
-        
+                onAccountClick = { accountId ->
                     navController.navigate(
-                        Routes.addTransaction(transactionId)
+                        Routes.editAccount(accountId)
                     )
                 }
             )
         }
-                composable(Routes.IMPORT) {
+
+        composable(Routes.ADD_ACCOUNT) {
+            AddAccountScreen(
+                onBack = { navController.popBackStack() },
+                onSaved = {
+                    navController.popBackStack()
+                }
+            )
+        }
+
+        composable(
+            route = Routes.EDIT_ACCOUNT,
+            arguments = listOf(
+                navArgument("accountId") {
+                    type = NavType.LongType
+                }
+            )
+        ) { backStackEntry ->
+
+            val accountId =
+                backStackEntry.arguments?.getLong("accountId")
+                    ?: return@composable
+
+            EditAccountScreen(
+                accountId = accountId,
+                onBack = { navController.popBackStack() }
+            )
+        }
+
+        composable(Routes.IMPORT) {
             ImportScreen(
                 onBack = { navController.popBackStack() }
             )
         }
+
         composable(Routes.SETTINGS) {
             SettingsScreen(
                 onBack = { navController.popBackStack() }
