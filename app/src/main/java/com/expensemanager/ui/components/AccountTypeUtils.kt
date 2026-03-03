e5fa0936f042b0533bee111544c0b44ba36ea845
Rajeev Bansal <rajivbansal36325@gmail.com>
2026-03-03 19:19:18 +0530

    Create AccountTypeUtils.kt

---
+++ app/src/main/java/com/expensemanager/ui/components/AccountTypeUtils.kt
@@ -0,0 +1,12 @@
+package com.expensemanager.ui.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+
+fun getAccountIcon(type: String) = when (type) {
+    "Cash" -> Icons.Default.Payments
+    "Credit card" -> Icons.Default.CreditCard
+    "Loan" -> Icons.Default.AccountBalance
+    "Investment" -> Icons.Default.TrendingUp
+    else -> Icons.Default.AccountBalanceWallet
+}
