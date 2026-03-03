6c11122db873bcf2ca80854be1ed2b24f736e5dc
Rajeev Bansal <rajivbansal36325@gmail.com>
2026-03-03 22:12:05 +0530

    Refactor AccountRepository to use AccountWithBalance

--- app/src/main/java/com/expensemanager/data/repository/AccountRepository.kt
+++ app/src/main/java/com/expensemanager/data/repository/AccountRepository.kt
@@ -2,6 +2,7 @@ package com.expensemanager.data.repository
 
 import com.expensemanager.data.local.dao.AccountDao
 import com.expensemanager.data.local.entities.AccountEntity
+import com.expensemanager.data.local.entities.AccountWithBalance
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -10,15 +11,19 @@ import javax.inject.Singleton
 class AccountRepository @Inject constructor(
     private val accountDao: AccountDao
 ) {
-    fun getAllAccounts(): Flow<List<AccountEntity>> = accountDao.getAllActiveAccounts()
 
-    fun getTotalBalance(): Flow<Double?> = accountDao.getTotalBalance()
+    fun getAllAccounts(): Flow<List<AccountWithBalance>> =
+        accountDao.getAccountsWithBalance()
 
-    suspend fun getAccountById(id: Long): AccountEntity? = accountDao.getAccountById(id)
+    suspend fun getAccountById(id: Long): AccountEntity? =
+        accountDao.getAccountById(id)
 
-    suspend fun insertAccount(account: AccountEntity): Long = accountDao.insertAccount(account)
+    suspend fun insertAccount(account: AccountEntity): Long =
+        accountDao.insert(account)
 
-    suspend fun updateAccount(account: AccountEntity) = accountDao.updateAccount(account)
+    suspend fun updateAccount(account: AccountEntity) =
+        accountDao.update(account)
 
-    suspend fun deleteAccount(account: AccountEntity) = accountDao.deleteAccount(account)
+    suspend fun deleteAccount(account: AccountEntity) =
+        accountDao.delete(account)
 }
