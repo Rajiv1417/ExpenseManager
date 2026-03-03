4c0618185351cffd3ed631cb5763798cb9be32ae
Rajeev Bansal <rajivbansal36325@gmail.com>
2026-03-04 00:10:53 +0530

    Create AccountType.kt

---
+++ app/src/main/java/com/expensemanager/data/local/entities/AccountType.kt
@@ -0,0 +1,12 @@
+package com.expensemanager.data.local.entities
+
+enum class AccountType {
+    CASH,
+    BANK,
+    CREDIT_CARD,
+    SAVINGS,
+    WALLET,
+    INVESTMENT,
+    LOAN,
+    OTHER
+}
