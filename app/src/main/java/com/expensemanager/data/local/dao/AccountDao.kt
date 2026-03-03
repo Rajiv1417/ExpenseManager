9eb47695f2129ee2ae65cc828498c228f23570a6
Rajeev Bansal <rajivbansal36325@gmail.com>
2026-03-03 22:09:56 +0530

    Modify AccountDao to return account ID on insert

--- app/src/main/java/com/expensemanager/data/local/dao/AccountDao.kt
+++ app/src/main/java/com/expensemanager/data/local/dao/AccountDao.kt
@@ -2,13 +2,14 @@ package com.expensemanager.data.local.dao
 
 import androidx.room.*
 import com.expensemanager.data.local.entities.AccountEntity
+import com.expensemanager.data.local.entities.AccountWithBalance
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface AccountDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(account: AccountEntity)
+    suspend fun insert(account: AccountEntity): Long
 
     @Update
     suspend fun update(account: AccountEntity)
@@ -16,16 +17,21 @@ interface AccountDao {
     @Delete
     suspend fun delete(account: AccountEntity)
 
+    @Query("SELECT * FROM accounts WHERE id = :id")
+    suspend fun getAccountById(id: Long): AccountEntity?
+
     @Query("""
-        SELECT a.id,
-               a.name,
-               a.accountNumber,
-               a.type,
-               a.initialValue,
-               a.currency,
-               a.color,
-               a.createdAt,
-               (a.initialValue + IFNULL(
+        SELECT 
+            a.id,
+            a.name,
+            a.accountNumber,
+            a.type,
+            a.initialValue,
+            a.currency,
+            a.color,
+            a.createdAt,
+            (
+                a.initialValue + IFNULL(
                     SUM(
                         CASE
                             WHEN t.type = 'INCOME' THEN t.amount
@@ -33,9 +39,11 @@ interface AccountDao {
                             WHEN t.type = 'TRANSFER' THEN 0
                         END
                     ), 0
-               )) as balance
+                )
+            ) as balance
         FROM accounts a
-        LEFT JOIN transactions t ON a.id = t.accountId
+        LEFT JOIN transactions t 
+            ON a.id = t.accountId
         GROUP BY a.id
         ORDER BY a.createdAt DESC
     """)
