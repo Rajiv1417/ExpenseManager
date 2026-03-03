aff2e8378fd54c1af9dba3ce45d2b9b1faff042c
Rajeev Bansal <rajivbansal36325@gmail.com>
2026-03-03 23:30:33 +0530

    Update AppDatabase.kt

--- app/src/main/java/com/expensemanager/data/local/AppDatabase.kt
+++ app/src/main/java/com/expensemanager/data/local/AppDatabase.kt
@@ -2,16 +2,28 @@ package com.expensemanager.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.expensemanager.data.local.dao.AccountDao
+import com.expensemanager.data.local.dao.TransactionDao
+import com.expensemanager.data.local.dao.CategoryDao
 import com.expensemanager.data.local.entities.AccountEntity
+import com.expensemanager.data.local.entities.TransactionEntity
+import com.expensemanager.data.local.entities.CategoryEntity
+import com.expensemanager.data.local.converters.Converters
 
 @Database(
-    entities = [AccountEntity::class,
-               TransactionEntity::class,
-               CategoryEntity::class],
+    entities = [
+        AccountEntity::class,
+        TransactionEntity::class,
+        CategoryEntity::class
+    ],
     version = 1,
     exportSchema = false
 )
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun accountDao(): AccountDao
+    abstract fun transactionDao(): TransactionDao
+    abstract fun categoryDao(): CategoryDao
 }
